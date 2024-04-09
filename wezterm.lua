f0b88c46888296090ef5a7e59c938e36b9cc1b3d
Fan-Yu Kuan <fykuan@gmail.com>
2024-04-09 15:31:43 +0800

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,16 +1,26 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
+local config = {}
 
-return {
-    initial_rows = 48,
+config = {
     initial_cols = 160,
+    initial_rows = 50,
     color_scheme = 'Tomorrow Night Eighties',
     font = wezterm.font_with_fallback {
         'Hack',
         'PingFang'
     },
-    window_background_opacity = 0.8,
-    text_background_opacity = 0.8,
+    window_background_opacity = 1.0,
+    text_background_opacity = 1.0,
     use_fancy_tab_bar = true,
-    scroll_back_lines = 1500,
+    hide_tab_bar_if_only_one_tab = true,
+    scrollback_lines = 1500,
 }
+
+config.colors = {
+    tab_bar = {
+        -- inactive_tab_edge = '#000000',
+    }
+}
+
+return config
