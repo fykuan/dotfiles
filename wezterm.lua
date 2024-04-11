647c54ebd8a380a0559b173036f4e2c761139c23
Fan-Yu Kuan <fykuan@gmail.com>
2024-04-11 16:11:15 +0800

    Update WezTerm config

--- wezterm.lua
+++ wezterm.lua
@@ -3,17 +3,21 @@ local act = wezterm.action
 local config = {}
 
 config = {
-    initial_cols = 160,
-    initial_rows = 50,
-    color_scheme = 'Tomorrow Night Eighties',
     font = wezterm.font_with_fallback {
         'Hack',
         'PingFang'
     },
-    window_background_opacity = 1.0,
-    text_background_opacity = 1.0,
+    color_scheme = 'Tomorrow Night Eighties',
+    -- Window size controls
+    initial_cols = 160,
+    initial_rows = 50,
+    -- Tab controls
     use_fancy_tab_bar = true,
     hide_tab_bar_if_only_one_tab = true,
+    --Apperence controls
+    text_background_opacity = 1.0,
+    window_background_opacity = 1.0,
+    --Scrolling controls
     scrollback_lines = 1500,
 }
 
